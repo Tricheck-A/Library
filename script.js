cb85256bcf10dc27d48bc596b105735ee0c41f14
Tricheck-A <94442823+Tricheck-A@users.noreply.github.com>
2022-09-22 13:20:16 +0200

    Add addBooks Container
    - addBooks Container pops up when clicking on the "add Book" Button
    - background gets blurred and darkened

--- script.js
+++ script.js
@@ -7,3 +7,31 @@ function Book() {
 function addBookToLibrary() {
   // do stuff here
 }
+
+
+
+
+
+// VARIABLES TO INTERACT WITH THE WEBSITE
+
+let addBookButton = document.getElementById("addBookButton");
+let addBookOverlay = document.getElementById("addBookOverlay");
+
+const blurObjs = Array.from(
+  document.getElementsByClassName('blur')
+);
+
+
+
+addBookButton.addEventListener("click", function(){
+
+  //blurs the background
+  blurObjs.forEach(obj => {
+    obj.style.filter = "blur(3px)";
+    obj.style.webkitFilter = "blur(3px)";
+  });
+
+  //displays the addBook window
+  addBookOverlay.style.display = "flex"
+
+});
